ae08a8d3c4f1da53ac2893bbfde294d2adf24d4a
l1l <khalil23ar@gmail.com>
2025-02-15 15:25:39 +0100

    Adding movement logic

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,5 +1,8 @@
-function select_one(selector) {
+function selectOne(selector) {
   return document.querySelector(selector);
+}
+function selectAll(selector) {
+  return document.querySelectorAll(selector);
 }
 function addClass(ele, cssClass) {
   ele.classList.add(cssClass);
@@ -7,14 +10,46 @@ function addClass(ele, cssClass) {
 function removeClass(ele, cssClass) {
   ele.classList.remove(cssClass);
 }
-const closeBtn = select_one(".cancel"),
-  form = select_one("form"),
-  addBtn = select_one(".add");
 
-closeBtn.addEventListener("click", () => {
+function addEvent(ele, event, handler) {
+  ele.addEventListener(event, handler);
+}
+
+function removeEvent(element, event, handler) {
+  element.removeEventListener(event, handler);
+}
+
+const closeBtn = selectOne(".cancel"),
+  form = selectOne("form"),
+  addBtn = selectOne(".add");
+
+addEvent(addBtn, "click", () => {
+  addClass(form, "active");
+});
+
+addEvent(closeBtn, "click", () => {
   removeClass(form, "active");
 });
 
-addBtn.addEventListener("click", () => {
-  addClass(form, "active");
+const move_eles = selectAll(".move"),
+  notes = selectAll(".note"),
+  moveImg = selectOne(".move img");
+
+move_eles.forEach((ele, key) => {
+  let isMoving = false;
+
+  const moveHandler = (event) => {
+    notes[key].style.left = event.clientX - 100 + "px";
+    notes[key].style.top = event.clientY - 25 + "px";
+  };
+
+  addEvent(ele, "click", () => {
+    isMoving = !isMoving;
+
+    if (isMoving) {
+      addEvent(document, "mousemove", moveHandler);
+    } else {
+      removeEvent(document, "mousemove", moveHandler);
+    }
+  });
 });
